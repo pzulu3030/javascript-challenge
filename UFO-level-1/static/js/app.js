b3f4077366a3bb8e2052048283977ef074782654
pparashar68 <pparashar68@gmail.com>
2020-12-18 12:21:56 -0800

    Added filtered data logic

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,11 +2,31 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 let table = d3.select("table"); // selected the table from html page
 table.attr("class", "table table-striped table-hover");
+
+// Capture form input
+button = d3.select("#filter-btn");
+button.on("click", runHandler);
+formInput = d3.select(".form-control");
+//formInput = d3.select(".form-control");
+//document.querySelector("#filter-btn").addEventListener("click", runHandler);
+
+// Create handlers
+
+function runHandler() {
+  //console.log(document.querySelector(".form-control").value);
+  inputElement = formInput.property("value");
+  console.log(inputElement);
+  if (!inputElement) {
+    tableData.forEach(ufoData);
+  } else {
+    filteredData = tableData.filter((item) => item.datetime === inputElement);
+    filteredData.forEach(ufoData);
+  }
+}
 
 function ufoData(item) {
   let row = tbody.append("tr");
@@ -18,5 +38,3 @@ function ufoData(item) {
   row.append("td").text(item.durationMinutes);
   row.append("td").text(item.comments);
 }
-
-tableData.forEach(ufoData);
