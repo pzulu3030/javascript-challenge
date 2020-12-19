56f5c05e5d65ee5451bdaca3961a1b31c1646706
pparashar68 <pparashar68@gmail.com>
2020-12-18 18:43:18 -0800

    Added logic to display filtered data and clear table before displaying new query results

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,28 +3,36 @@ var tableData = data;
 
 // YOUR CODE HERE!
 // Get a reference to the table body
-var tbody = d3.select("tbody");
+let tbody = d3.select("tbody");
 let table = d3.select("table"); // selected the table from html page
 table.attr("class", "table table-striped table-hover");
 
 // Capture form input
+// Clean displayed previous query displayed data
+formInput = d3.select(".form-control");
+formInput.on("change", cleanDisplay);
+
+function cleanDisplay() {
+  tbody.html("");
+}
+
 button = d3.select("#filter-btn");
 button.on("click", runHandler);
 formInput = d3.select(".form-control");
-//formInput = d3.select(".form-control");
-//document.querySelector("#filter-btn").addEventListener("click", runHandler);
 
 // Create handlers
-
 function runHandler() {
   //console.log(document.querySelector(".form-control").value);
+
   inputElement = formInput.property("value");
   console.log(inputElement);
   if (!inputElement) {
     tableData.forEach(ufoData);
   } else {
     filteredData = tableData.filter((item) => item.datetime === inputElement);
+    console.log(filteredData);
     filteredData.forEach(ufoData);
+    // tbody.html("");
   }
 }
 
